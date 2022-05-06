bab3b1e50502ff95cde19d47a8a72840aa31e44a
Sulav Tuladhar <sulav.tuladhar@dev.itglance.org>
2022-05-06 14:21:03 +0545

    importing & loading inbuilt & third party middlewares

--- app.js
+++ app.js
@@ -3,7 +3,35 @@ const app = express();
 const PORT = 9090;
 
 // Importing api router
-// const API_ROUTER = require('./api_route');
+const API_ROUTER = require('./api_route');
+
+// Third party middleware 
+const morgan = require('morgan');
+const cors = require('cors');
+
+// Loading third party middleware
+app.use(cors());
+app.use(morgan('dev'))
+
+// Loading inbuilt middleware
+app.use(express.static('uploads')); //Serves static files
+app.use(express.urlencoded({
+    // Parser for x-ww-form-urlencoded
+    extended: true
+}));
+app.use(express.json());
+app.use('/files', express.static('uploads'));
+
+
+app.use('/api', API_ROUTER);
+
+// 404 error handler 
+app.use(function(req,res,next){
+    next({
+        msg: "Page not found",
+        status: 404
+    })
+})
 
 // Error handleing middleware
 app.use(function(err,req,res,next){
