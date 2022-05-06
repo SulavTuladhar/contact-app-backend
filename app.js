bb133fdb3651e30a0c5544ae36cf7704a0bdf4a2
Sulav Tuladhar <sulav.tuladhar@dev.itglance.org>
2022-05-06 14:16:53 +0545

    error handling middleware

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = 9090;
 
+// Importing api router
+// const API_ROUTER = require('./api_route');
+
+// Error handleing middleware
+app.use(function(err,req,res,next){
+    res.status(err.status || 400);
+    res.json({
+        msg: err.msg || err,
+        status: err.status || 400
+    })
+})
+
 app.listen(PORT, function(err,done){
     if(err){
         console.log('Server listening failed ', err);
